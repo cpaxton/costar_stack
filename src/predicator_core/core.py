973898907c3e3272812ebfac0af2937abae83afc
Christopher Paxton <cpaxton3@jhu.edu>
2014-06-12 12:10:00 -0400

    changed core to publish versions of predicates with 1 free variable, plus empty versions of some predicates so you can, for example, get all objects that are touching.

--- src/predicator_core/core.py
+++ src/predicator_core/core.py
@@ -25,7 +25,11 @@ def get_key(predicate, params):
             params[1],
             params[2])
 
-
+'''
+get a predicate out of a string
+'''
+def get_predicate_from_key(key):
+    pass
 
 '''
 Predicator()
@@ -34,18 +38,18 @@ Aggregates lists of predicates arriving on a list topic, and publishes them.
 '''
 class Predicator(object):
 
-    def __init__(self, sub_topic, pub_topic):
+    def __init__(self, sub_topic, pub_topic, test_srv, get_srv):
         self._subscriber = rospy.Subscriber(sub_topic, PredicateList, self.callback)
         self._publisher = rospy.Publisher(pub_topic, PredicateSet)
-        self._testService = rospy.Service('predicator/test_predicate',TestPredicate, self.test_predicate)
-        self._getService = rospy.Service('predicator/get_assignment',GetAssignment, self.get_assignment)
+        self._testService = rospy.Service(test_srv,TestPredicate, self.test_predicate)
+        self._getService = rospy.Service(get_srv,GetAssignment, self.get_assignment)
         self._latest = {}
+        self._predicates = {}
 
     def callback(self, msg):
         self._latest[msg.header.frame_id] = msg.statements
 
     def aggregate(self):
-        print self._latest
         d = {}
         for source, lst in self._latest.items():
             for predicate in lst:
@@ -54,22 +58,63 @@ class Predicator(object):
 
                 for j in range(predicate.num_params):
                     new_params = copy.deepcopy(predicate.params)
-                    free_vars = ['','','']
+                    #free_vars = ['','','']
 
                     new_params[j] = ''
-                    free_vars[j] = predicate.params[j]
+                    #free_vars[j] = predicate.params[j]
 
                     new_key = get_key(predicate.predicate, new_params)
 
                     if not new_key in d:
                         d[new_key] = []
-                    d[new_key].append(get_key('',free_vars))
+                    d[new_key].append(copy.deepcopy(predicate.params))
 
-    def get_assignment(self, req):
+                if(predicate.num_params > 1):
+                    pred_key = get_key(predicate.predicate, ['','',''])
+                    if not pred_key in d:
+                        d[pred_key] = []
+                    d[pred_key].append(copy.deepcopy(predicate.params))
+                
+        self._predicates = d
+
+    '''
+    publish a message with all true things
+    '''
+    def publish(self):
         pass
 
+    '''
+    test_predicate (SERVICE)
+    Check to see if a certain predicate exists on the server
+    '''
     def test_predicate(self, req):
-        pass
+        if get_key(req.statement.predicate, req.statement.params) in self._predicates:
+            print "Found predicate in response to request:"
+            print req
+            return TestPredicateResponse(found=True)
+        else:
+            print "Did not find predicate in response to request:"
+            print req
+            return TestPredicateResponse(found=False)
+
+    '''
+    get_assignment (SERVICE)
+    get possible assignments of free variables
+    '''
+    def get_assignment(self, req):
+        key = get_key(req.statement.predicate, req.statement.params) 
+        if key in self._predicates:
+            vals = []
+            for stored_params in self._predicates[key]:
+                s = PredicateStatement(predicate=req.statement.predicate,
+                        num_params=req.statement.num_params,
+                        params=stored_params)
+                vals.append(s)
+            return GetAssignmentResponse(found=True, values=vals)
+        else:
+            print "Did not find predicate in response to request:"
+            print req
+            return GetAssignmentResponse(found=False, values=[])
 
 if __name__ == '__main__':
     rospy.init_node('predicator_core')
@@ -80,10 +125,14 @@ if __name__ == '__main__':
 
     try:
 
-        pc = Predicator('predicator/input', 'predicator/output')
+        pc = Predicator('predicator/input',
+                'predicator/output',
+                'predicator/test_predicate',
+                'predicator/get_assignment')
 
         while not rospy.is_shutdown():
             pc.aggregate()
+            pc.publish()
             rate.sleep()
 
     except rospy.ROSInterruptException: pass
