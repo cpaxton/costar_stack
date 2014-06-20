f2f511819eb88c9364c24b5fc1d16b0a8aaaa463
Christopher Paxton <cpaxton3@jhu.edu>
2014-06-20 17:15:42 -0400

    added in predicates for nearness, in the XY plane and in 3D space, and for left/right/up/down/front/back relative to a point of view determined by some other coordinate frame.

--- src/predicator_geometry/predicator_geometry_node.py
+++ src/predicator_geometry/predicator_geometry_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
 import tf
 
 from predicator_msgs.msg import *
@@ -13,13 +14,17 @@ class GeometryPredicator(object):
         self._frames = rospy.get_param('~frames')
         self._world_frame = rospy.get_param('~world_frame','world')
         self._higher_margin = rospy.get_param('~height_threshold',0.1)
-        print self._frames
+        self._x_threshold = rospy.get_param('~rel_x_threshold',0.1)
+        self._y_threshold = rospy.get_param('~rel_y_threshold',0.1)
+        self._z_threshold = rospy.get_param('~rel_z_threshold',0.1)
+        self._near_2d_threshold = rospy.get_param('~near_2D_threshold',0.2)
+        self._near_3d_threshold = rospy.get_param('~near_3D_threshold',0.2)
 
     '''
     addHeightPredicates()
     Determines if some objects are higher than others.
     Can be passed a margin/threshold, in case things aren't different enough to matter.
-    higher(A, B) means A is higher than B
+    higher_than(A, B) means B is higher than A
     '''
     def addHeightPredicates(self, msg, frame1, frame2):
         
@@ -31,17 +36,17 @@ class GeometryPredicator(object):
 
             if height_difference > self._higher_margin:
                 ps = PredicateStatement()
-                ps.predicate = 'higher'
-                ps.params[0] = frame2
-                ps.params[1] = frame1
+                ps.predicate = 'higher_than'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
                 ps.num_params = 2
                 ps.value = height_difference
                 msg.statements.append(ps)
             elif height_difference < -1 * self._higher_margin:
                 ps = PredicateStatement()
-                ps.predicate = 'lower'
-                ps.params[0] = frame2
-                ps.params[1] = frame1
+                ps.predicate = 'lower_than'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
                 ps.num_params = 2
                 ps.value = height_difference
                 msg.statements.append(ps)
@@ -50,6 +55,57 @@ class GeometryPredicator(object):
 
         return msg
 
+    '''
+    addNearPredicates()
+    Are the two frames close together in the (X, Y) world plane?
+    Are the two frames close together in all (X, Y, Z) coordinates?
+    '''
+    def addNearPredicates(self, msg, frame1, frame2):
+        try:
+            (trans1, rot1) = self._listener.lookupTransform(self._world_frame, frame1, rospy.Time(0))
+            (trans2, rot2) = self._listener.lookupTransform(self._world_frame, frame2, rospy.Time(0))
+
+            dx = trans2[0] - trans1[0]
+            dy = trans2[1] - trans1[1]
+            dz = trans2[2] - trans1[2]
+
+            dist = np.sqrt((dx*dx)+(dy*dy))
+            dist_xyz = np.sqrt((dx*dx)+(dy*dy)+(dz*dz))
+
+            if dist <= self._near_2d_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'near_xy'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.num_params = 2
+                ps.value = dist
+                msg.statements.append(ps)
+            if dist_xyz <= self._near_3d_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'near_xyz'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.num_params = 2
+                ps.value = dist
+                msg.statements.append(ps)
+
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException): pass
+
+        return msg
+
+
+    '''
+    addRelativeDirectionPredicates()
+    Adds predicates indicating that another object is in a certain direction from a given object.
+    Also needs a frame of reference, which can be anything.
+    Frames of reference include the objects involved.
+
+    Example:
+    left_of(A,B,C) means B is left from A, from the perspective of C.
+    
+    In the context of the peg demo, this might mean that the robot arm is to "stage left" of a peg.
+    '''
     def addRelativeDirectionPredicates(self, msg, frame1, frame2, ref):
 
         try:
@@ -59,6 +115,63 @@ class GeometryPredicator(object):
             x_diff = trans2[0] - trans1[0]
             y_diff = trans2[1] - trans1[1]
             z_diff = trans2[2] - trans1[2]
+
+            if x_diff > self._x_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'in_front_of'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = x_diff
+                msg.statements.append(ps)
+            elif x_diff < -1 * self._x_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'in_back_of'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = x_diff
+                msg.statements.append(ps)
+
+            if y_diff > self._y_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'left_of'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = y_diff
+                msg.statements.append(ps)
+            elif y_diff < -1 * self._y_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'right_of'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = y_diff
+                msg.statements.append(ps)
+
+            if z_diff > self._z_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'up_from'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = z_diff
+                msg.statements.append(ps)
+            elif z_diff < -1 * self._z_threshold:
+                ps = PredicateStatement()
+                ps.predicate = 'down_from'
+                ps.params[0] = frame1
+                ps.params[1] = frame2
+                ps.params[2] = ref
+                ps.num_params = 3
+                ps.value = z_diff
+                msg.statements.append(ps) 
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException): pass
 
@@ -81,6 +194,10 @@ class GeometryPredicator(object):
                 else:
                     # check height predicates
                     msg = self.addHeightPredicates(msg, frame1, frame2)
+                    msg = self.addNearPredicates(msg, frame1, frame2)
+
+                    for ref in self._frames:
+                        msg = self.addRelativeDirectionPredicates(msg, frame1, frame2, ref)
 
         return msg
 
@@ -106,7 +223,6 @@ if __name__ == "__main__":
 
         while not rospy.is_shutdown():
             msg = gp.getPredicateMessage()
-            print msg
             gp.publish(msg)
             rate.sleep()
 
