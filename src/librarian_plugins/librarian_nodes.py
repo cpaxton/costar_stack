e901d0bff43383d746df4d46bfbd7b10a3c4168c
Christopher Paxton <cpaxton3@jhu.edu>
2014-06-27 10:36:43 -0400

    added text boxes; fill in text box with top level librarian types. TODO: figure out the best way to do callbacks

--- src/librarian_plugins/librarian_nodes.py
+++ src/librarian_plugins/librarian_nodes.py
@@ -10,59 +10,140 @@ from PyQt4.QtCore import *
 import beetree; from beetree import Node
 from instructor import NodeGUI
 from instructor.instructor_qt import NamedField
+from instructor.instructor_qt import NamedComboBox
 # For testing the service node
 from instructor_plugins.srv import *
+
+from threading import Thread
+
+from librarian_msgs.msg import *
+from librarian_msgs.srv import *
+
+
+lister = rospy.ServiceProxy('/librarian/list', List)
+saver = rospy.ServiceProxy('/librarian/save', Save)
+loader = rospy.ServiceProxy('/librarian/load', Load)
+param_loader = rospy.ServiceProxy('/libarian/load', Load)
+
+'''
+Get a list in a folder (or empty)
+'''
+def getList(folder):
+    return lister(folder)
+
+'''
+# create a thread class to keep things up to date
+class MyThread(QtCore.QThread):
+    updated = QtCore.pyqtSignal(str)
+
+    def __init__(self) 
+
+    def run( self ):
+        # do some functionality
+        if len(gui.type_.get()) > 0:
+            file_list = getList(gui.type_.get())
+            gui.file_.add_items(file_list.entries)
+
+    def stop (self ):
+        self.exit()
+'''
 
 # Sample Node Wrappers -----------------------------------------------------------
 class LoadParametersGUI(NodeGUI):
     def __init__(self):
         super(LoadParametersGUI,self).__init__()
 
+        self.type_ = NamedComboBox('Type')
+        self.file_ = NamedComboBox('Name')
+        self.layout_.addWidget(self.type_)
+        self.layout_.addWidget(self.file_)
+        
+        type_list = getList('')
+        self.type_.add_items(type_list.entries)
+
+        if len(self.type_.get()) > 0:
+            file_list = getList(self.type_.get())
+            self.file_.add_items(file_list.entries)
+
     def generate(self,parent=None):
         if all([self.name.full(),self.label.full()]):
-            return LoadParameters(parent,self.get_name(),self.get_label())
+            return LoadParameters(parent,self.get_name(),self.get_label(),
+                    self.type_.get(), self.file_.get())
         else:
             return 'ERROR: node not properly defined'
 
 class LoadTextFromFileGUI(NodeGUI):
     def __init__(self):
         super(LoadTextFromFileGUI,self).__init__()
+        self.type_ = NamedComboBox('Type')
+        self.file_ = NamedComboBox('Name')
+        self.layout_.addWidget(self.type_)
+        self.layout_.addWidget(self.file_)
+
+        type_list = getList('')
+        self.type_.add_items(type_list.entries)
+
+
+
 
     def generate(self,parent=None):
         if all([self.name.full(),self.label.full()]):
-            return LoadText(parent,self.get_name(),self.get_label())
+            return LoadText(parent,self.get_name(),self.get_label(),
+                    self.type_.get(), self.file_.get())
         else:
             return 'ERROR: node not properly defined'
 
 class SaveParameterGUI(NodeGUI):
     def __init__(self):
         super(SaveParameterGUI,self).__init__()
+        self.type_ = NamedComboBox('Type')
+        self.file_ = NamedComboBox('Name')
+        self.layout_.addWidget(self.type_)
+        self.layout_.addWidget(self.file_)
+        self.param_ = NamedField('Parameter','')
+        self.layout_.addWidget(self.param_)
+
+        type_list = getList('')
+        self.type_.add_items(type_list.entries)
 
     def generate(self,parent=None):
         if all([self.name.full(),self.label.full()]):
-            return SaveParameter(parent,self.get_name(),self.get_label())
+            return SaveParameter(parent,self.get_name(),self.get_label(),
+                    self.type_.get(), self.file_.get(), self.param_.get())
         else:
             return 'ERROR: node not properly defined'
 
 class LoadEntryFromFileGUI(NodeGUI):
     def __init__(self):
         super(LoadEntryFromFileGUI,self).__init__()
+        self.type_ = NamedComboBox('Type')
+        self.file_ = NamedComboBox('Name')
+        self.entry_ = NamedField('Entry','')
+        self.layout_.addWidget(self.type_)
+        self.layout_.addWidget(self.file_)
+        self.layout_.addWidget(self.entry_)
+        type_list = getList('')
+        self.type_.add_items(type_list.entries)
 
     def generate(self,parent=None):
-        if all([self.name.full(),self.label.full()]):
-            return LoadEntry(parent,self.get_name(),self.get_label())
+        if all([self.name.full(),self.label.full(), self.entry_.full()]):
+            return LoadEntry(parent,self.get_name(),self.get_label(),
+                    self.type_.get(), self.file_.get(), self.entry_.get())
         else:
             return 'ERROR: node not properly defined'
 
 # Sample Nodes -------------------------------------------------------------------
 class LoadText(Node):
-    def __init__(self,parent,name,label):
+    def __init__(self,parent,name,label,folder,fname):
         color='#5B8EEB'
         super(LoadText,self).__init__(False,parent,name,label,color)
+        self.folder_ = folder
+        self.name_ = fname
+
     def get_node_type(self):
         return 'SERVICE'
     def get_node_name(self):
-        return 'Service'
+        return 'Load Text from File'
     def execute(self):
         # TODO
         print 'Executing Service: ' + self.name_
@@ -71,13 +152,17 @@ class LoadText(Node):
         return self.node_status_
 
 class LoadEntry(Node):
-    def __init__(self,parent,name,label):
+    def __init__(self,parent,name,label,folder,fname,entry):
         color='#5B8EEB'
         super(LoadEntry,self).__init__(False,parent,name,label,color)
+        self.folder_ = folder
+        self.name_ = fname
+        self.entry_ = entry
+
     def get_node_type(self):
         return 'SERVICE'
     def get_node_name(self):
-        return 'Service'
+        return 'Load Entry from Text File'
     def execute(self):
         # TODO
         print 'Executing Service: ' + self.name_
@@ -85,17 +170,18 @@ class LoadEntry(Node):
         print '  -  Node: ' + self.name_ + ' returned status: ' + self.node_status_
         return self.node_status_
 
-from threading import Thread
 class LoadParameters(Node):
-    def __init__(self,parent,name,label):
+    def __init__(self,parent,name,label,folder,fname):
         super(LoadParameters,self).__init__(False,parent,name,label,'#92D665')
         self.service_thread = Thread(target=self.make_service_call, args=('request',1))
         self.running = False
         self.finished_with_success = None
+        self.folder_ = folder
+        self.name_ = fname
     def get_node_type(self):
         return 'SERVICE'
     def get_node_name(self):
-        return 'Service'
+        return 'Load Parameters From File'
 
     def execute(self):
         if not self.running: # Thread is not running
@@ -129,17 +215,19 @@ class LoadParameters(Node):
             print e
             self.finished_with_success = False
 
-from threading import Thread
 class SaveParameter(Node):
-    def __init__(self,parent,name,label):
+    def __init__(self,parent,name,label,folder,fname,param):
         super(SaveParameter,self).__init__(False,parent,name,label,'#92D665')
         self.service_thread = Thread(target=self.make_service_call, args=('request',1))
         self.running = False
         self.finished_with_success = None
+        self.folder_ = folder
+        self.name_ = fname
+        self.param_ = param
     def get_node_type(self):
         return 'SERVICE'
     def get_node_name(self):
-        return 'Service'
+        return 'Save Parameter to File'
 
     def execute(self):
         if not self.running: # Thread is not running
