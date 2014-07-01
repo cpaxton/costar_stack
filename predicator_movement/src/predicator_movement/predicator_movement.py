25575dcefd2bbbb5984e2460b8f315add6772394
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-01 14:03:13 -0400

    movement predicates -- defining input, parameters, functions, plus an example configuration

--- predicator_movement/src/predicator_movement/predicator_movement.py
+++ predicator_movement/src/predicator_movement/predicator_movement.py
@@ -10,6 +10,45 @@ from predicator_msgs import *
 class MovementPredicator(object):
 
     def __init__(self, topic, valid_topic):
+        self._listener = tf.TransformListener()
+        self._publisher = rospy.Publisher(topic, PredicateList)
+        self._valid_publisher = rospy.Publisher(valid_topic, ValidPredicates)
+        self._frames = rospy.get_param('~frames')
+        self._world_frame = rospy.get_param('~world_frame','world')
+        self._trans_threshold = rospy.get_param('~translation_velocity_threshold', 0.01)
+        self._rot_threshold = rospy.get_param('~rotation_velocity_threshold', 0.01)
+        self._s0 = 0
+        self._s1 = 0
+        pass
+
+    '''
+    update()
+    Read in more messages
+    '''
+    def update(self):
+        try:
+            for frame in self._frames:
+
+                # get position in world coordinates
+                (trans, rot) = self._listener.lookupTransform(self._world_frame, frame, rospy.Time(0))
+
+                # 
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            pass
+        
+        pass
+
+    '''
+    getMessage()
+    '''
+    def getMessage(self):
+        pass
+
+    '''
+    getValidMessage()
+    '''
+    def getValidMessage(self):
         pass
 
 if __name__ == "__main__":
