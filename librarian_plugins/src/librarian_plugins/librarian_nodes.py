db8c3a80fd3614b8286e58940eb8b92622d1e770
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-02 15:24:48 -0400

    added librarian plugins and node implementations for most things

--- librarian_plugins/src/librarian_plugins/librarian_nodes.py
+++ librarian_plugins/src/librarian_plugins/librarian_nodes.py
@@ -33,24 +33,15 @@ Get a list in a folder (or empty)
 def getList(folder):
     return lister(folder)
 
-'''
-# create a thread class to keep things up to date
-class MyThread(QtCore.QThread):
-    updated = QtCore.pyqtSignal(str)
-
-    def __init__(self) 
-
-    def run( self ):
-        # do some functionality
-        if len(gui.type_.get()) > 0:
-            file_list = getList(gui.type_.get())
-            gui.file_.add_items(file_list.entries)
-
-    def stop (self ):
-        self.exit()
-'''
-
 # Sample Node Wrappers -----------------------------------------------------------
+
+'''
+LoadParametersGUI
+load parameters from a file
+uses the librarian/load_params service
+
+(written on 2014/07/02)
+'''
 class LoadParametersGUI(NodeGUI):
     def __init__(self):
         super(LoadParametersGUI,self).__init__()
@@ -60,20 +51,48 @@ class LoadParametersGUI(NodeGUI):
         self.layout_.addWidget(self.type_)
         self.layout_.addWidget(self.file_)
         
-        type_list = getList('')
-        self.type_.add_items(type_list.entries)
+        self.type_list = getList('')
+        self.type_.add_items(self.type_list.entries)
+        self.type_.interface().currentIndexChanged.connect(self.selected)
+        self.file_list = []
 
+        if len(self.type_.get()) > 0:
+            self.file_list = getList(self.type_list.entries[int(self.type_.get())])
+            self.file_.add_items(self.file_list.entries)
+
+    def selected(self,t):
+        value = str(t)
+        self.file_list = getList(self.type_list.entries[int(self.type_.get())])
+        self.file_.interface().clear()
+        self.file_.add_items(self.file_list.entries)
         if len(self.type_.get()) > 0:
             file_list = getList(self.type_.get())
             self.file_.add_items(file_list.entries)
 
     def generate(self,parent=None):
+
+        if len(self.file_list.entries) > 0:
+            fname = self.file_list.entries[int(self.file_.get())]
+        else:
+            return 'ERROR: no items found!'
+
+        if len(self.type_list.entries) > 0:
+            tname = self.type_list.entries[int(self.type_.get())]
+        else:
+            return 'ERROR: no types found!'
         if all([self.name.full(),self.label.full()]):
             return LoadParameters(parent,self.get_name(),self.get_label(),
-                    self.type_.get(), self.file_.get())
+                    tname, fname)
         else:
             return 'ERROR: node not properly defined'
 
+'''
+LoadTextFromFileGUI()
+get both the folder and item name from drop down boxes
+send to node
+
+(written 2014/07/02)
+'''
 class LoadTextFromFileGUI(NodeGUI):
     def __init__(self):
         super(LoadTextFromFileGUI,self).__init__()
@@ -93,19 +112,39 @@ class LoadTextFromFileGUI(NodeGUI):
 
     def selected(self,t):
         value = str(t)
-        print self.type_.get()
         self.file_list = getList(self.type_list.entries[int(self.type_.get())])
         self.file_.interface().clear()
         self.file_.add_items(self.file_list.entries)
-        print "selected"
 
     def generate(self,parent=None):
+
+        if len(self.file_list.entries) > 0:
+            fname = self.file_list.entries[int(self.file_.get())]
+        else:
+            return 'ERROR: no items found!'
+
+        if len(self.type_list.entries) > 0:
+            tname = self.type_list.entries[int(self.type_.get())]
+        else:
+            return 'ERROR: no types found!'
+
+        print fname
+        print tname
+
         if all([self.name.full(),self.label.full()]):
             return LoadText(parent,self.get_name(),self.get_label(),
-                    self.type_.get(), self.file_.get())
+                    fname, tname)
         else:
             return 'ERROR: node not properly defined'
 
+'''
+SaveParameterGUI
+takes in text fields to get parameters
+gets names
+gets ids
+
+(written on 2014/07/02)
+'''
 class SaveParameterGUI(NodeGUI):
     def __init__(self):
         super(SaveParameterGUI,self).__init__()
@@ -123,12 +162,25 @@ class SaveParameterGUI(NodeGUI):
         #self.param_.add_items(rosparam.get_params(''))
 
     def generate(self,parent=None):
+
+        if len(self.type_list.entries) > 0:
+            tname = self.type_list.entries[int(self.type_.get())]
+        else:
+            return 'ERROR: no types found!'
+
         if all([self.name.full(),self.label.full()],self.file_.full()):
             return SaveParameter(parent,self.get_name(),self.get_label(),
-                    self.type_.get(), self.file_.get(), self.param_.get())
+                    tname, self.file_.get(), self.param_.get())
         else:
             return 'ERROR: node not properly defined'
 
+'''
+LoadEntryFromFileGUI
+loads a text entry from a dictionary in a single configuration file somewhere
+not really a high priority to implement this
+
+(written on 2014/07/02)
+'''
 class LoadEntryFromFileGUI(NodeGUI):
     def __init__(self):
         super(LoadEntryFromFileGUI,self).__init__()
@@ -138,13 +190,32 @@ class LoadEntryFromFileGUI(NodeGUI):
         self.layout_.addWidget(self.type_)
         self.layout_.addWidget(self.file_)
         self.layout_.addWidget(self.entry_)
-        type_list = getList('')
-        self.type_.add_items(type_list.entries)
+        self.type_list = getList('')
+        self.type_.add_items(self.type_list.entries)
+        self.type_.interface().currentIndexChanged.connect(self.selected)
+        self.file_list = []
+
+    def selected(self,t):
+        value = str(t)
+        self.file_list = getList(self.type_list.entries[int(self.type_.get())])
+        self.file_.interface().clear()
+        self.file_.add_items(self.file_list.entries)
 
     def generate(self,parent=None):
+
+        if len(self.file_list.entries) > 0:
+            fname = self.file_list.entries[int(self.file_.get())]
+        else:
+            return 'ERROR: no items found!'
+
+        if len(self.type_list.entries) > 0:
+            tname = self.type_list.entries[int(self.type_.get())]
+        else:
+            return 'ERROR: no types found!'
+
         if all([self.name.full(),self.label.full(), self.entry_.full()]):
             return LoadEntry(parent,self.get_name(),self.get_label(),
-                    self.type_.get(), self.file_.get(), self.entry_.get())
+                    tname, fname, self.entry_.get())
         else:
             return 'ERROR: node not properly defined'
 
@@ -161,7 +232,6 @@ class LoadText(Node):
     def get_node_name(self):
         return 'Load Text from File'
     def execute(self):
-        # TODO
         print 'Executing Service: ' + self.name_
         self.node_status_ = 'SUCCESS'
         print '  -  Node: ' + self.name_ + ' returned status: ' + self.node_status_
@@ -186,6 +256,12 @@ class LoadEntry(Node):
         print '  -  Node: ' + self.name_ + ' returned status: ' + self.node_status_
         return self.node_status_
 
+'''
+LoadParameters
+Loads parameters to ROS with a Librarian service call
+
+(written as of 2014/07/02)
+'''
 class LoadParameters(Node):
     def __init__(self,parent,name,label,folder,fname):
         super(LoadParameters,self).__init__(False,parent,name,label,'#92D665')
@@ -222,15 +298,21 @@ class LoadParameters(Node):
                     self.running = False
 
     def make_service_call(self,request,*args):
-        rospy.wait_for_service('instructor_plugins/test_service')
+        rospy.wait_for_service('librarian/load_params')
         try:
-            test_service = rospy.ServiceProxy('instructor_plugins/test_service', AddTwoInts)
-            self.result = test_service(request)
+            test_service = rospy.ServiceProxy('librarian/load_params', librarian_msgs.srv.LoadParams)
+            self.result = test_service(id=self.name_, type=self.folder_)
             self.finished_with_success = True
         except rospy.ServiceException, e:
             print e
             self.finished_with_success = False
 
+'''
+SaveParameter
+Take a single thing off the ROS parameter server and put it in a file
+
+(written as of 2014/07/02)
+'''
 class SaveParameter(Node):
     def __init__(self,parent,name,label,folder,fname,param):
         super(SaveParameter,self).__init__(False,parent,name,label,'#92D665')
@@ -268,10 +350,11 @@ class SaveParameter(Node):
                     self.running = False
 
     def make_service_call(self,request,*args):
-        rospy.wait_for_service('instructor_plugins/test_service')
+        rospy.wait_for_service('librarian/save')
         try:
-            test_service = rospy.ServiceProxy('instructor_plugins/test_service', AddTwoInts)
-            self.result = test_service(request)
+            val = rosparam.get_param(self.param_)
+            test_service = rospy.ServiceProxy('librarian/save', librarian_msgs.srv.Save)
+            self.result = test_service(type=self.folder_, id=self.name_, text=val)
             self.finished_with_success = True
         except rospy.ServiceException, e:
             print e
