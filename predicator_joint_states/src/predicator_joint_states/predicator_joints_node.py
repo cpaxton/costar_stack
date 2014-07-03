e44eba63c5c6ad9f2653860c857036d0a69a9ca8
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-03 17:19:47 -0400

    predicator joint states things

---
+++ predicator_joint_states/src/predicator_joint_states/predicator_joints_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import *
+from predicator_msgs.msg import *
+
+if __name__ == "__main__":
+    rospy.init_node("predicator_joints_node")
+    pub = rospy.Publisher('predicator/input', PredicateList)
+    vpub = rospy.Publisher('predicator/valid_input', ValidPredicates)
+
+    rate = rospy.Rate(30)
+    
+    # read in some information
+    config = rospy.get_param('~js_config')
+    print config
+
+
