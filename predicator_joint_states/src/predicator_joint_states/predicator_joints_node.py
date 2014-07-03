34702b803b0d027f0d89e70757b9c8b71d2182b3
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-03 17:59:23 -0400

    wam arms simple joint check node

--- predicator_joint_states/src/predicator_joint_states/predicator_joints_node.py
+++ predicator_joint_states/src/predicator_joint_states/predicator_joints_node.py
@@ -4,6 +4,25 @@ import rospy
 from sensor_msgs.msg import *
 from predicator_msgs.msg import *
 
+js = []
+
+def cb(msg):
+    #print msg.position
+    js = [x for x in msg.position]
+
+    msg = PredicateList()
+    ps = PredicateStatement()
+    ps.predicate = 'is_closed'
+    ps.num_params = 1
+    ps.params = [config['id'],'','']
+    msg.statements.append(ps)
+
+
+    if len(js) > 0 and js[idx] < threshold:
+        #print js[idx]
+        #print threshold
+        pub.publish(msg)
+
 if __name__ == "__main__":
     rospy.init_node("predicator_joints_node")
     pub = rospy.Publisher('predicator/input', PredicateList)
@@ -15,10 +34,15 @@ if __name__ == "__main__":
     config = rospy.get_param('~js_config')
     print config
 
+    sub = rospy.Subscriber(config['topic'], JointState, cb)
+    threshold = float(config['thresholds'])
+    idx = int(config['idx'])
+
     while not rospy.is_shutdown():
 
         # publish predicates message
-        # publish 
+        # publish
+
 
         rate.sleep()
 
