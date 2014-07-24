4248e7d66d5c427ac8e04f0584669fdb9449b5d5
Colin Lea <colincsl@gmail.com>
2014-07-24 12:17:29 -0400

    Updated occupancy gui. Now based on sphere instead of bounding box.

--- predicator_occupancy_module/src/predicator_occupancy_module/pick_volume.py
+++ predicator_occupancy_module/src/predicator_occupancy_module/pick_volume.py
@@ -134,7 +134,7 @@ def get_bounding_box_sliders():
         cv2.waitKey(30)
 
 
-def learn_volume():
+def learn_volume_bounding_box():
     """ User clicks on a bounding box in the color image to specify the volume """
     global drawing_box, drawing_box_done, box_center, bbox_img
     global waiting_state, pause_video
@@ -209,6 +209,164 @@ def learn_volume():
     return bounding_box
 
 
+def click_to_pause():
+    global pause_video, waiting_state
+
+    txt = "Click once to pause the video"
+    print txt
+    cv2.setMouseCallback("learn_volume", pause_video_callback)
+    waiting_state = True
+    pause_video = False
+    while not pause_video:
+        img_pause_rgb = im_rgb.copy()
+        img_pause_pos = im_pos.copy()
+
+        img = img_pause_rgb.copy().astype(np.int)
+        img[:40] = np.maximum(img[:40]-100, 0)
+        img = img.astype(np.uint8)
+
+        cv2.putText(img, txt, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+        cv2.imshow("learn_volume", img)
+        if cv2.waitKey(30) >= 0:
+            break
+
+    # Wait until mouse up
+    while waiting_state:
+        if cv2.waitKey(30) >= 0:
+            break
+
+    return img_pause_rgb, img_pause_pos
+
+
+def click_point_of_interest(img_pause_rgb, img_pause_pos):
+    global box_center, waiting_state
+
+    cv2.setMouseCallback("learn_volume", center_callback)
+
+    txt = "Click the center of your volume"
+    print txt
+
+    waiting_state = True
+    mask = np.any(img_pause_pos != 0, -1)
+    while box_center[0] == 0:
+        img = img_pause_rgb.copy().astype(np.int)
+        img[:40] = np.maximum(img[:40]-100, 0)
+        img = img.astype(np.uint8)
+
+        img *= mask[:, :, None]
+        img[:, :, 2][~mask] += 100
+        cv2.putText(img, txt, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+        cv2.imshow("learn_volume", img)
+
+        if cv2.waitKey(30) >= 0:
+            break
+
+    # Wait until mouse up
+    while waiting_state:
+        if cv2.waitKey(30) >= 0:
+            break
+
+    rgbd_center = img_pause_pos[box_center[1], box_center[0]]
+
+    return rgbd_center
+
+
+def mask_radius(img_pause_pos, rgbd_center, radius):
+    img_radius = np.sqrt(np.nansum(((img_pause_pos - rgbd_center)**2).reshape([-1, 3]), -1))
+    mask = img_radius < radius
+    mask = mask.reshape(img_pause_pos.shape[:2])
+
+    return mask
+
+
+def recolor_mask(img, mask):
+    # Recolor in/out of radius regions
+    img = np.maximum(img.astype(np.int)-100, 0)
+
+    # Color in-radius green
+    img[:, :, 1] += mask*100
+
+    # Color out-of-radius red
+    img[:, :, 2] += (~mask)*100
+    img = img.astype(np.uint8)
+
+    return img
+
+
+def click_radius(img_pause_rgb, img_pause_pos, rgbd_center):
+
+    # Step 3: Select radius for sensor
+    txt = "Select radius for occupancy sensor"
+    txt2 = "Then press enter"
+    empty_fcn = lambda _: None
+    cv2.createTrackbar("Radius", "learn_volume", 0, 2000, empty_fcn)
+
+    while 1:
+        radius = cv2.getTrackbarPos("Radius", 'learn_volume')
+        radius = max(1, radius)/1000.
+        print "Radius:", radius
+
+        mask = mask_radius(img_pause_pos, rgbd_center, radius)
+        img = img_pause_rgb.copy()
+        img = recolor_mask(img, mask)
+
+        cv2.putText(img, txt, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+        cv2.putText(img, txt2, (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+        cv2.imshow("learn_volume", img)
+
+        if cv2.waitKey(30) >= 0:
+            break
+
+    return radius
+
+
+def verify_occupancy(rgbd_center, radius):
+    global pause_video, waiting_state, img_pos
+
+    txt = "Click to verify. Press escape to redo"
+    print txt
+    cv2.setMouseCallback("learn_volume", pause_video_callback)
+    waiting_state = True
+    pause_video = False
+    while not pause_video:
+        img_pause_pos = im_pos.copy()
+        mask = mask_radius(img_pause_pos, rgbd_center, radius)
+
+        img = im_rgb.copy().astype(np.int)
+        img[:40] = np.maximum(img[:40]-100, 0)
+        img = img.astype(np.uint8)
+        img = recolor_mask(img, mask)
+
+        cv2.putText(img, txt, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+        cv2.imshow("learn_volume", img)
+        if cv2.waitKey(30) >= 0:
+            return False
+            break
+
+    return True
+
+
+def learn_volume_sphere():
+    """ User clicks on a bounding box in the color image to specify the volume """
+    global drawing_box, drawing_box_done, box_center
+
+    box_center = [0, 0]
+    verified = False
+
+    while not verified:
+        cv2.destroyAllWindows()
+        cv2.namedWindow("learn_volume")
+
+        img_pause_rgb, img_pause_pos = click_to_pause()
+        rgbd_center = click_point_of_interest(img_pause_rgb, img_pause_pos)
+        radius = click_radius(img_pause_rgb, img_pause_pos, rgbd_center)
+        verified = verify_occupancy(rgbd_center, radius)
+
+    cv2.destroyAllWindows()
+
+    return rgbd_center, radius
+
+
 def pointcloud_callback(data):
     """
     data : ros msg data
@@ -218,11 +376,8 @@ def pointcloud_callback(data):
     im_pos = np.dstack([img_pos_raw['x'], img_pos_raw['y'], img_pos_raw['z']])
     im_pos = np.nan_to_num(im_pos)
 
-    im_rgb = np.dstack([x['r'], x['g'], x['b']])
+    im_rgb = np.dstack([img_pos_raw['r'], img_pos_raw['g'], img_pos_raw['b']])
     im_rgb = cv2.cvtColor(im_rgb, cv2.COLOR_BGR2RGB)
-
-    # frame_time = data.header.stamp
-    # frame_seq = data.header.seq
 
 
 if __name__ == '__main__':
@@ -238,8 +393,8 @@ if __name__ == '__main__':
         rate.sleep()
 
     # Have user add volume
-    volume = learn_volume()
-    rospy.set_param("volume", volume)
-
-    print "Volume added:"
-    print volume
+    # volume = learn_volume()
+    # rospy.set_param("volume", volume)
+    center, radius = learn_volume_sphere()
+    rospy.set_param("occupancy_center", center.tolist())
+    rospy.set_param("occupancy_radius", radius)
