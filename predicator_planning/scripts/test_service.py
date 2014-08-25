d34902c6173c7898a5eef4a654e2ac4829b5f224
Christopher Paxton <cpaxton3@jhu.edu>
2014-08-25 15:08:47 -0400

    planning service tester; seems to work

--- predicator_planning/scripts/test_service.py
+++ predicator_planning/scripts/test_service.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
-from predicator_planning.srv import PredicatePlan
+from predicator_planning.srv import *
+from trajectory_msgs.msg import JointTrajectory
 
+rospy.init_node('test_predicate_planning_node')
+planner = rospy.ServiceProxy('predicator/plan', PredicatePlan)
+pub = rospy.Publisher('gazebo/traj_rml/joint_traj_cmd', JointTrajectory)
 
+req = PredicatePlanRequest()
+req.group = 'arm'
+req.robot = 'wam'
+res = planner(req)
 
+print res
 
+pub.publish(res.path)
