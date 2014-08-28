98acde9780b5fc80a40ab55abbaa3c0596076e4a
Christopher Paxton <cpaxton3@jhu.edu>
2014-08-28 16:55:38 -0400

    volume size parameter in main file; accessed as ros parameter

--- predicator_planning/src/predicator_planning/predicator_planning_node.cpp
+++ predicator_planning/src/predicator_planning/predicator_planning_node.cpp
@@ -18,14 +18,16 @@ int main(int argc, char **argv) {
   double step = 0.10;
   double chance = 0.30;
   double skip_distance = 0.75;
+  double search_volume = 0.50;
 
   ros::NodeHandle nh("~");
   nh.param("max_iter", max_iter, int(5000));
   nh.param("step", step, double(0.10));
   nh.param("chance", chance, double(0.30));
   nh.param("skip_distance", skip_distance, double(0.75));
+  nh.param("search_volume", search_volume, double(0.50));
 
-  predicator_planning::Planner planner(&pc, (unsigned int)max_iter, step, chance, skip_distance);
+  predicator_planning::Planner planner(&pc, (unsigned int)max_iter, step, chance, skip_distance, search_volume);
 
   // define spin rate
   ros::Rate rate(30);
