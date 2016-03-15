9fd6a30cd2777e0380eb273d7fe61734c9b9352a
Chris Paxton <cpaxton3@jhu.edu>
2016-03-15 10:52:34 -0400

    adding python setup, driver to send messages to KUKA bridge

--- costar_robot/simple_iiwa_driver/scripts/iiwa_driver.py
+++ costar_robot/simple_iiwa_driver/scripts/iiwa_driver.py
@@ -1,3 +1,14 @@
-import rospy
+#!/usr/bin/env python
 
+import rospy
+from simple_iiwa_driver import *
+
+rospy.init_node('simple_iiwa_driver')
+driver = SimpleIIWADriver()
+
+rate = rospy.Rate(60)
+
+while not rospy.is_shutdown():
+  driver.tick()
+  rate.sleep()
 
--- costar_robot/simple_iiwa_driver/src/simple_iiwa_driver/driver.py
+++ costar_robot/simple_iiwa_driver/src/simple_iiwa_driver/driver.py
@@ -1,31 +1,49 @@
-import rospy
 
-from costar_robot_msgs import *
+import rospy
+from costar_robot_msgs.srv import *
+from std_msgs.msg import String
+from trajectory_msgs.msg import JointTrajectoryPoint
+from std_srvs.srv import Empty as EmptyService
+
+mode = {'TEACH':'TeachArm', 'SERVO':'MoveArmJointServo', 'SHUTDOWN':'ShutdownArm'}
+
 
 class SimpleIIWADriver:
 
     def __init__(self):
-        self.teach_mode = rospy.Service('simple_robot/TeachMode',SetTeachMode,set_teach_mode_call)
-        self.servo_mode = rospy.Service('simple_robot/ServoMode',SetServoMode,set_servo_mode_call)
-        self.driver_state = 'NONE'
+        self.teach_mode = rospy.Service('costar/SetTeachMode',SetTeachMode,self.set_teach_mode_call)
+        self.servo_mode = rospy.Service('costar/SetServoMode',SetServoMode,self.set_servo_mode_call)
+        self.shutdown = rospy.Service('costar/ShutdownArm',EmptyService,self.shutdown_arm_call)
+        self.driver_status = 'IDLE'
+        self.status_publisher = rospy.Publisher('costar/DriverStatus',String,queue_size=1000)
+        self.iiwa_mode_publisher = rospy.Publisher('interaction_mode',String,queue_size=1000)
+        self.pt_publisher = rospy.Publisher('joint_traj_pt_cmd',JointTrajectoryPoint,queue_size=1000)
 
     def set_teach_mode_call(self,req):
         if req.enable == True:
             # self.rob.set_freedrive(True)
-            self.driver_status = 'TeachArm'
+            self.driver_status = 'TEACH'
             return 'SUCCESS - teach mode enabled'
         else:
             # self.rob.set_freedrive(False)
-            self.driver_status = 'NONE'
+            self.driver_status = 'IDLE'
             return 'SUCCESS - teach mode disabled'
 
     def set_servo_mode_call(self,req):
         if req.mode == 'SERVO':
-            self.driver_status = 'MoveArmJointServo'
+            self.driver_status = 'SERVO'
             return 'SUCCESS - servo mode enabled'
         elif req.mode == 'DISABLE':
             self.driver_status = 'IDLE'
             return 'SUCCESS - servo mode disabled'
 
     def shutdown_arm_call(self,req):
+        self.driver_status = 'SHUTDOWN'
         pass
+
+    def tick(self):
+        self.status_publisher.publish(self.driver_status)
+        if self.driver_status in mode.keys():
+            self.iiwa_mode_publisher.publish(mode[self.driver_status])
+        else:
+            rospy.logwarn('IIWA mode for %s not specified!'%self.driver_status)
