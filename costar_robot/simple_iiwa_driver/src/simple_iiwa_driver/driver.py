a88ce350b22c8b0ca0b61263c80508939aab7023
Chris Paxton <cpaxton3@jhu.edu>
2016-03-16 14:21:45 -0400

    iiwa driver

--- costar_robot/simple_iiwa_driver/src/simple_iiwa_driver/driver.py
+++ costar_robot/simple_iiwa_driver/src/simple_iiwa_driver/driver.py
@@ -14,10 +14,32 @@ class SimpleIIWADriver:
         self.teach_mode = rospy.Service('costar/SetTeachMode',SetTeachMode,self.set_teach_mode_call)
         self.servo_mode = rospy.Service('costar/SetServoMode',SetServoMode,self.set_servo_mode_call)
         self.shutdown = rospy.Service('costar/ShutdownArm',EmptyService,self.shutdown_arm_call)
+        self.servo = rospy.Service('costar/ServoToPose',ServoToPose,self.servo_to_pose_call)
         self.driver_status = 'IDLE'
         self.status_publisher = rospy.Publisher('costar/DriverStatus',String,queue_size=1000)
         self.iiwa_mode_publisher = rospy.Publisher('interaction_mode',String,queue_size=1000)
         self.pt_publisher = rospy.Publisher('joint_traj_pt_cmd',JointTrajectoryPoint,queue_size=1000)
+
+    def servo_to_pose_call(self,req): 
+        if self.driver_status == 'SERVO':
+            T = tf_c.fromMsg(req.target)
+            a,axis = T.M.GetRotAngle()
+            pose = list(T.p) + [a*axis[0],a*axis[1],a*axis[2]]
+            # Check acceleration and velocity limits
+            if req.accel > self.MAX_ACC:
+                acceleration = self.MAX_ACC
+            else:
+                acceleration = req.accel
+            if req.vel > self.MAX_VEL:
+                velocity = self.MAX_VEL
+            else:
+                velocity = req.vel
+            # Send command
+            self.rob.movel(pose,acc=acceleration,vel=velocity)
+            return 'SUCCESS - moved to pose'
+        else:
+            rospy.logerr('SIMPLE UR -- Not in servo mode')
+            return 'FAILED - not in servo mode'
 
     def set_teach_mode_call(self,req):
         if req.enable == True:
