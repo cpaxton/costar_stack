969f72ecb6422e146167a011064f09b2581051ba
Andrew Hundt <ATHundt@gmail.com>
2016-03-29 16:02:44 -0400

    adding

---
+++ costar_tools/object_scanning_tools/src/costar/__init__.py
@@ -0,0 +1,1 @@
+from record_trajectory import *
---
+++ costar_tools/object_scanning_tools/src/costar/record_trajectory.py
@@ -0,0 +1,8 @@
+import rospy
+from sensor_msgs.msg import *
+from trajectory_msgs.msg import *
+
+class TrajectoryRecorder:
+    
+    def __init__(self):
+        pass
