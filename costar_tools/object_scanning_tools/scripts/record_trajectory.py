2d8bf5f35a760bebab4ab470fa131c082eb76740
Chris Paxton <cpaxton3@jhu.edu>
2016-03-29 21:45:45 -0400

    recording trajectories on key press

--- costar_tools/object_scanning_tools/scripts/record_trajectory.py
+++ costar_tools/object_scanning_tools/scripts/record_trajectory.py
@@ -7,8 +7,8 @@ rospy.init_node('costar_trajectory_recorder_node')
 filename = rospy.get_param('~filename','trajectory.yml')
 
 tr = TrajectoryRecorder()
-
-rospy.spin()
+tr.record(filename)
+#rospy.spin()
 
 print 'Done. Saving to file "%s"...'%(filename)
 tr.save(filename)
--- costar_tools/object_scanning_tools/src/costar/record_trajectory.py
+++ costar_tools/object_scanning_tools/src/costar/record_trajectory.py
@@ -3,22 +3,31 @@ import rosbag
 from sensor_msgs.msg import *
 from trajectory_msgs.msg import *
 import yaml
+import curses
 
 class TrajectoryRecorder:
     
+    last_msg = None
     
     def __init__(self):
         self.sub = rospy.Subscriber('joint_states',JointState,self.cb)
         self.traj = JointTrajectory()
+        self.last_msg = None
             
     def cb(self, msg):
-        pt = JointTrajectoryPoint()
-        pt.positions = msg.position
-        pt.velocities = msg.velocity
-        pt.effort = msg.effort
-        self.traj.points.append(pt)
-        #print self.traj
-        print " - %d points"%(len(self.traj.points))
+        self.last_msg = msg
+
+    def update(self):
+
+        if not self.last_msg is None:
+            pt = JointTrajectoryPoint()
+            pt.positions = self.last_msg.position
+            pt.velocities = self.last_msg.velocity
+            pt.effort = self.last_msg.effort
+            self.traj.points.append(pt)
+            #print self.traj
+
+            self.last_msg = None
 
     def save(self, filename):
         with open(filename,'w') as f:
@@ -28,3 +37,25 @@ class TrajectoryRecorder:
         with open(filename,'r') as f:
             print yaml.load(f)
             
+    def record(self, filename):
+        stdscr = curses.initscr()
+        count = 0
+        stdscr.addstr(0,0,"Press any key to record.")
+        rate = rospy.Rate(30)
+        try:
+            while not rospy.is_shutdown():
+                count += 1
+                key = stdscr.getch()
+                self.update()
+                stdscr.addstr(count,0," - %d points"%(len(self.traj.points)))
+                rate.sleep()
+        except rospy.ROSInterruptException, e:
+            stdscr.addstr(count+1,0,"Finishing...")
+        finally:
+            self.save(filename)
+            curses.endwin()
+
+    def load(self, filename):
+        with open(filename,'r') as f:
+            self.traj = yaml.load(f)
+        
