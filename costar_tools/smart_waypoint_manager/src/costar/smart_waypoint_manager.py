edf195ca0be9f2778e3e84688ef521891bcd07b8
Chris Paxton <cpaxton3@jhu.edu>
2016-04-01 10:30:39 -0400

    adding smart waypoint manager class

---
+++ costar_tools/smart_waypoint_manager/src/costar/smart_waypoint_manager.py
@@ -0,0 +1,18 @@
+import rospy
+from predicator_msgs.srv import *
+from predicator_msgs.msg import *
+from librarian_msgs.srv import *
+from librarian_msgs.msg import *
+
+'''
+SmartWaypointManager
+This class will create and load smart waypoints from the $LIBRARIAN_HOME/smart_waypoints directory
+It also provides ways to use and access these methods
+'''
+class SmartWaypointManager:
+
+
+    def __init__(self,world="world"):
+        self.get_waypoints_srv = GetWaypointsService(world=world,service=False)
+        pass
+
