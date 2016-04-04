2d187290ce226792cb8fe831af6b5960eb8703f2
Chris Paxton <cpaxton3@jhu.edu>
2016-04-04 11:46:23 -0400

    adding simple iiwa driver smart move test

--- costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
+++ costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
@@ -10,3 +10,5 @@ rospy.init_node('test_smart_move_node')
 obj_class = "node_uniform" # based on object detection output
 right_of = PredicateStatement(predicate="right_of",params=['*','ar_marker_2','world'])
 
+
+
