7c8500c4200e560049331b946b02d8b7bae18b53
Chris Paxton <cpaxton3@jhu.edu>
2016-04-04 17:26:40 -0400

    adding file to test movements

--- costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
+++ costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
@@ -5,10 +5,16 @@ import rospy
 from predicator_msgs.msg import *
 from predicator_msgs.srv import *
 
+from simple_iiwa_driver import *
+from simple_planning import *
+
 rospy.init_node('test_smart_move_node')
 
 obj_class = "node_uniform" # based on object detection output
 right_of = PredicateStatement(predicate="right_of",params=['*','ar_marker_2','world'])
 
+driver = SimpleIIWADriver()
+planner = SimplePlanning()
 
+rate = rospy.Rate(60)
 
