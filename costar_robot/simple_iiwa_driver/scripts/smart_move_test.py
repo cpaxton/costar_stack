3ee45c3722edf6564619337b95b6aa4cc6f3cc65
Chris Paxton <cpaxton3@jhu.edu>
2016-04-04 21:01:17 -0400

    setting up smart move service to move to goals -- try to move to the closest one to the current arm configuration

--- costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
+++ costar_robot/simple_iiwa_driver/scripts/smart_move_test.py
@@ -8,15 +8,33 @@ from predicator_msgs.srv import *
 from simple_iiwa_driver import *
 from simple_planning import *
 from smart_waypoint_manager import *
+from geometry_msgs.msg import Pose
 
 rospy.init_node('test_smart_move_node')
 
 obj_class = "node_uniform" # based on object detection output
 right_of = PredicateStatement(predicate="right_of",params=['*','ar_marker_2','world'])
+pose = Pose()
+pose.position.x = 0.05
+pose.position.y = -0.35
+pose.position.z = 0.01
+pose.orientation.x = -0.63
+pose.orientation.y = -0.77
+pose.orientation.z = 0.02
+pose.orientation.w = 0.07
 
 driver = SimpleIIWADriver()
-planner = SimplePlanning()
 swm = SmartWaypointManager()
+
+rospy.sleep(2.0)
 
 rate = rospy.Rate(60)
 
+req = SmartMoveRequest()
+req.predicates = [right_of]
+req.obj_class = obj_class
+req.pose = pose
+
+print req
+
+print driver.smart_move_call(req)
