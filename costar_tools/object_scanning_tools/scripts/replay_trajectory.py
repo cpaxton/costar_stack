ac4d1236c06bb3d29ae51b377c5055a11531fa02
Chris Paxton <cpaxton3@jhu.edu>
2016-04-12 16:43:13 -0400

    adding hz arg

--- costar_tools/object_scanning_tools/scripts/replay_trajectory.py
+++ costar_tools/object_scanning_tools/scripts/replay_trajectory.py
@@ -5,12 +5,13 @@ from costar import TrajectoryRecorder
 
 rospy.init_node('costar_trajectory_replayer_node')
 filename = rospy.get_param('~filename','trajectory.yml')
+hz = rospy.get_param('~hz',0.2)
 
 tr = TrajectoryRecorder()
 tr.load(filename)
 #rospy.spin()
 
 print "Playing..."
-tr.play()
+tr.play(hz=hz)
 print "Done."
 
