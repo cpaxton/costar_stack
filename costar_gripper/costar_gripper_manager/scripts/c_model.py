e70f762d347e1663016487d02f3e09deb752847c
Chris Paxton <cpaxton3@jhu.edu>
2016-05-19 15:33:54 -0400

    c model gripper now on correct topic

--- costar_gripper/costar_gripper_manager/scripts/c_model.py
+++ costar_gripper/costar_gripper_manager/scripts/c_model.py
@@ -5,7 +5,7 @@ from costar_gripper import SimpleCModelServer
 
 rospy.init_node("simple_c_model_server")
 verbose = rospy.get_param('~verbose',False)
-server = SimpleCModelServer("c_model",verbose=verbose)
+server = SimpleCModelServer("costar/gripper",verbose=verbose)
 server.open_gripper()
 #server.basic_mode()
 rospy.spin()
