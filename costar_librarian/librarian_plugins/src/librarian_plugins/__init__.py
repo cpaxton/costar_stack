8a4f3bddcff71832194cdd0695d479b56a2ca551
Adam Petrone <adampetrone83@gmail.com>
2016-08-19 17:53:11 -0400

    Librarian doesn't need plugins; especially with requirements on NodeGUI. (#3)

--- costar_librarian/librarian_plugins/src/librarian_plugins/__init__.py
+++ costar_librarian/librarian_plugins/src/librarian_plugins/__init__.py
@@ -1,15 +1,3 @@
 ### ROS imports
 import roslib; roslib.load_manifest('librarian_plugins')
 import rospy
-
-### Available Classes for 'from beetree import *'
-__all__ = ['LoadParametersGUI']
-__all__ += ['SaveParameterGUI']
-__all__ += ['LoadTextFromFileGUI']
-__all__ += ['LoadEntryFromFileGUI']
-
-### Sample Nodes
-from librarian_nodes import LoadParametersGUI
-from librarian_nodes import SaveParameterGUI
-from librarian_nodes import LoadTextFromFileGUI
-from librarian_nodes import LoadEntryFromFileGUI
