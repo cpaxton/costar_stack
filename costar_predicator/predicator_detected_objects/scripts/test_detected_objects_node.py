003a70d28c67f6b2b9175f2b83ae421a1667a69a
Chris Paxton <cpaxton3@jhu.edu>
2016-09-08 11:21:49 -0400

    publish detected objects

--- costar_predicator/predicator_detected_objects/scripts/test_detected_objects_node.py
+++ costar_predicator/predicator_detected_objects/scripts/test_detected_objects_node.py
@@ -9,14 +9,20 @@ ls = DetectedObjectList()
 #obj1 = DetectedObject(id="gbeam_link_1/gbeam_link",object_class="link")
 #obj2 = DetectedObject(id="gbeam_node_1/gbeam_node",object_class="node")
 #obj3 = DetectedObject(id="gbeam_node_2/gbeam_node",object_class="node")
-obj1 = DetectedObject(id="Obj/link_uniform/1",object_class="link_uniform")
-obj2 = DetectedObject(id="Obj/node_uniform/1",object_class="node_uniform")
-obj3 = DetectedObject(id="Obj/node_uniform/2",object_class="node_uniform")
+obj1 = DetectedObject(id="obj_link_uniform_1",object_class="link_uniform")
+obj2 = DetectedObject(id="obj_node_uniform_1",object_class="node_uniform")
+obj3 = DetectedObject(id="obj_node_uniform_2",object_class="node_uniform")
 obj1.symmetry.x_symmetries = 4
+obj1.symmetry.y_symmetries = 2
+obj1.symmetry.z_symmetries = 2
+obj2.symmetry.x_symmetries = 4
+obj2.symmetry.y_symmetries = 4
+obj2.symmetry.z_symmetries = 4
+obj3.symmetry.x_symmetries = 4
+obj3.symmetry.y_symmetries = 4
+obj3.symmetry.z_symmetries = 4
 
 ls.objects = [obj1,obj2,obj3]
-
-print ls
 
 rate = rospy.Rate(10)
 pub = rospy.Publisher("detected_object_list",DetectedObjectList,queue_size=1000)
