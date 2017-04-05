ca776529d0bb68642ac635e712e74c7639604549
Chris Paxton <cpaxton3@jhu.edu>
2017-04-05 13:49:23 -0700

    adding gripper manager

---
+++ costar_gripper/gripper_manager/src/gripper_manager/costar_gripper.py
@@ -0,0 +1,7 @@
+from costar_component import MyComponent
+
+# The CoSTAR Gripper component contains a lot less special logic than the
+# CoSTAR Arm does. It mostly just has a few tools.
+class CostarGripper(CostarComponent):
+    def __init__(self, *args, **kwargs):
+        pass
