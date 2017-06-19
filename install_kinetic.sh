3de8839f140a312cc6921504103ea5b2b11a58eb
Baichuan Jiang <bjiang8@jhu.edu>
2017-06-19 15:16:53 -0400

    update install scripts

--- install_kinetic.sh
+++ install_kinetic.sh
@@ -4,7 +4,8 @@
 cd ~
 mkdir -p costar_ws/src
 cd ~/costar_ws
-source /opt/ros/indigo/setup.bash
+export ROS_DISTRO=kinetic
+source /opt/ros/$ROS_DISTRO/setup.bash
 sudo apt-get update -qq
 sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-catkin
 catkin init
@@ -13,7 +14,7 @@ git clone https://github.com/cpaxton/costar_stack.git
 git clone https://github.com/SalvoVirga/iiwa_stack.git  
 git clone https://github.com/ros-industrial/robotiq.git  
 git clone https://github.com/jbohren/rqt_dot.git  
-git clone https://github.com/sniekum/ar_track_alvar.git --branch indigo-devel 
+git clone https://github.com/sniekum/ar_track_alvar.git --branch $ROS_DISTRO-devel 
 git clone https://github.com/sniekum/ar_track_alvar_msgs.git
 git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 git clone https://github.com/xqms/ur_modern_driver.git --branch thread_safety
