18b0575ad5d5b54154526480c0853c30d46c006c
Chris Paxton <cpaxton3@jhu.edu>
2017-07-11 10:47:14 -0700

    enable sp segmenter in master

--- install_indigo.sh
+++ install_indigo.sh
@@ -6,7 +6,15 @@ mkdir -p costar_ws/src
 cd ~/costar_ws
 source /opt/ros/indigo/setup.bash
 sudo apt-get update -qq
+
+# Set up your basic deps
 sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-catkin
+
+# Set up Opencv-nonfree
+sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo apt-get update 
+sudo apt-get install libopencv-nonfree-dev
+
 catkin init
 cd ~/costar_ws/src
 git clone https://github.com/cpaxton/costar_stack.git  
@@ -18,7 +26,5 @@ git clone https://github.com/sniekum/ar_track_alvar_msgs.git
 git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 git clone https://github.com/ThomasTimm/ur_modern_driver.git
 rosdep install -y --from-paths ./ --ignore-src --rosdistro $ROS_DISTRO
-echo "Ignore COSTAR_PERCEPTION until you have installed its dependencies."
-touch costar_stack/costar_perception/CATKIN_IGNORE
 catkin build --continue
 source ../devel/setup.bash
