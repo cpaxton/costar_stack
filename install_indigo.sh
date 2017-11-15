bed13a29d1f9837893d42fa461024abfa8aed180
Chris Paxton <paxton.chris@live.com>
2017-11-14 20:29:20 -0500

    install_indigo.sh ar_track_alvar moved
    https://github.com/ros-perception/ar_track_alvar
    
    The msgs version has been moved directly into the repo:
    git clone https://github.com/ros-perception/ar_track_alvar.git
    # git clone https://github.com/ros-perception/ar_track_alvar_msgs.git

--- install_indigo.sh
+++ install_indigo.sh
@@ -10,11 +10,6 @@ sudo apt-get update -qq
 # Set up your basic deps
 sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-catkin
 
-# Set up PCL 1.7.2
-sudo apt-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl 
-sudo apt-get update 
-sudo apt-get install libpcl-all 
-
 # Set up Opencv-nonfree
 sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
 sudo apt-get update 
@@ -27,8 +22,7 @@ git clone https://github.com/cpaxton/iiwa_stack.git
 git clone https://github.com/ros-industrial/robotiq.git  
 git clone https://github.com/jhu-lcsr/ObjRecRANSAC.git  
 git clone https://github.com/jbohren/rqt_dot.git  
-git clone https://github.com/sniekum/ar_track_alvar.git  
-git clone https://github.com/sniekum/ar_track_alvar_msgs.git  
+git clone https://github.com/ros-perception/ar_track_alvar.git
 git clone https://github.com/gt-ros-pkg/hrl-kdl.git
 git clone https://github.com/xqms/ur_modern_driver.git --branch thread_safety
 rosdep install -y --from-paths ./ --ignore-src --rosdistro $ROS_DISTRO
