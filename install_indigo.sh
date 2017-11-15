f966f9ccf383a055eac816b5741e81653eb45054
Felix Jonathan <fjonath1@users.noreply.github.com>
2017-11-14 20:54:14 -0500

    Update install_indigo.sh

--- install_indigo.sh
+++ install_indigo.sh
@@ -11,7 +11,7 @@ sudo apt-get update -qq
 sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-catkin
 
 # Set up PCL 1.7.2
-sudo apt-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl 
+sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl 
 sudo apt-get update 
 sudo apt-get install libpcl-all 
 
